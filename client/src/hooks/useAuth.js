da724c29b2c46132bd99c5a425d0587a11773f23
Nikki Lionel Ocer <nlocer97@gmail.com>
2024-10-14 19:12:35 +0800

    disable clearing cookies

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -13,7 +13,7 @@ const useAuth = () => {
   // Define handleUnauthenticated using useCallback to avoid stale closures
   const handleUnauthenticated = useCallback(() => {
     // Clear token (optional)
-    document.cookie = "whisprToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    // document.cookie = "whisprToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     setAuthData(null);
     navigate("/login"); // Redirect to login for all invalid token cases
   }, [navigate]); // Add navigate as a dependency
